f8862cb0ed89d11326dfabe87146d6e099d1eaba
EkaterinaFedotova <katyaF85@yandex.ru>
2023-03-02 20:49:59 +0300

    Fask 66

--- 66/Program.cs
+++ 66/Program.cs
@@ -1,6 +1,10 @@
 ﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму 
 //натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
 
+using System;
+using static System.Console;
 
 Clear();
 Write("Введите M: ");
@@ -8,10 +12,14 @@ int m = int.Parse(ReadLine());
 Write("Введите N: ");
 int n = int.Parse(ReadLine());
 
-WriteLine($"Cумма чисел = {SumNumbers(m, n)}");
+WriteLine($"Cумма натуральных элементов = {SumElements(m, n)}");
 
-int SumNumbers(int start, int end)
+int SumElements(int m, int n)
 {
-    if (start == end) return start;
-    return (start + SumNumbers(start + 1, end));
+    if (m == n)
+        return m;
+    else
+    {
+        return m + SumElements(m+1, n);
+    }
 }
