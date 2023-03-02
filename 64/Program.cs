e0a4d13d55dcb360e2f8aef7e09f3988267d273b
EkaterinaFedotova <katyaF85@yandex.ru>
2023-03-02 21:01:19 +0300

    Fask 64

--- 64/Program.cs
+++ 64/Program.cs
@@ -1,16 +1,28 @@
-﻿//Задайте значения M и N. Напишите программу, которая
-// выведет все натуральные числа по убыванию в промежутке от M до N
+﻿// Задайте значение N. Напишите программу, которая выведет все 
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
 
 Clear();
-Write("Введите M: ");
-int m = int.Parse(ReadLine());
 Write("Введите N: ");
 int n = int.Parse(ReadLine());
+int count = 1;
+NaturalNumbers(n, count);
 
-WriteLine(PrintNumbers(m, n));
 
-string PrintNumbers(int start, int end)
+void NaturalNumbers(int n, int count)
 {
-    if (end == start) return end.ToString();
-    return (end + " " + PrintNumbers(start, end - 1));
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalNumbers(n, count + 1);
+        Write(count + " ");
+    }
 }
