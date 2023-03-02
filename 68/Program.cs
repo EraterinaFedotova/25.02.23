de6b3fdf4304fd97815bdb08d387635412a5ccf8
EkaterinaFedotova <katyaF85@yandex.ru>
2023-03-02 21:17:50 +0300

    Fask 68

--- 68/Program.cs
+++ 68/Program.cs
@@ -1,17 +1,25 @@
-﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии.
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Она принимает два неотрицательных целых числа в качестве параметров 
+// и возвращает натуральное число, обозначается A(n, m).
+
+using System;
+using static System.Console;
 
 Clear();
 Write("Введите M: ");
-int a1 = int.Parse(ReadLine());
+int m = int.Parse(ReadLine());
 Write("Введите N: ");
-int a2 = int.Parse(ReadLine());
+int n = int.Parse(ReadLine());
 
-WriteLine($"Функция Аккермана = {Akkerman(a1, a2)}");
+WriteLine($"Функция Аккермана = {Akkerman(m, n)}");
 
-int Akkerman(int m, int n)
+int Akkerman(int i, int k)
 {
-    if (m == 0) return n + 1;
-    if (n == 0) return Akkerman(m - 1, 1);
-    return Akkerman(m - 1, Akkerman(m, n - 1));
+  if (i == 0) return k + 1;
+  else if (k == 0) return Akkerman(i - 1, 1);
+  else return Akkerman(i - 1, Akkerman(i, k - 1));
 }
